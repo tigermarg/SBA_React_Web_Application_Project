31c7b8e085033663be986080519d745e4f839eeb
tigermarg <178945478+tigermarg@users.noreply.github.com>
2024-11-13 14:00:25 -0500

    Added CSS

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,10 +1,10 @@
 export default function BookCard({ image, title, author, published }){
     return(
-        <>
+        <div className="book-card">
             <img src={image} alt=""/>
             <h3>{title}</h3>
             <p>{author}</p>
             <p>{published}</p>
-        </>
+        </div>
     )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,8 +2,8 @@ export default function SearchBar({ handleSearch, search , onClick}) {
 
   return (
     <>
-      <form action="">
-          <input onChange={handleSearch} value={search} type="text" placeholder="Search for a book..."/>
+      <form className="searchBar" action="">
+          <input className="searchBox" onChange={handleSearch} value={search} type="text" placeholder="Search for a book..."/>
           <button onClick={onClick}>Search</button>
       </form>
     </>
