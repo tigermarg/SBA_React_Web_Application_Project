1a3ea84ecb800c7c5621951087b0fb030f5b86b9
tigermarg <178945478+tigermarg@users.noreply.github.com>
2024-11-15 10:32:43 -0500

    added useEffect

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchBar from "./SearchBar";
 import Library from './Library';
 
@@ -6,6 +6,7 @@ export default function Body() {
 
     const [books, setBooks] = useState([]) //State for books
     const [search, setSearch] = useState('') //State for search
+    const [triggerSearch, setTriggerSearch] = useState(false) //Set state for manual search
 
   // Async function to fetch books based on the search term
   async function searchBook() {
@@ -15,13 +16,23 @@ export default function Body() {
       const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`);
 
       const data = await response.json();
-      console.log(data)
+
       setBooks(data.items || [])
 
     } catch (err) {
       console.error(err); 
     }
   }
+
+    // useEffect to trigger searchBook when search term changes
+    useEffect(() => {
+      if (search || triggerSearch) {
+          searchBook();
+          setTriggerSearch(false); // Reset trigger after search
+      } else {
+          setBooks([]); // Clear books if search is empty
+      }
+    }, [search, triggerSearch]); // Dependency on search and triggerSearch
     
     //Function to update search state when input changes
     function handleChange(e){
@@ -31,7 +42,7 @@ export default function Body() {
     //Function for click event
     function handleClick(e){
         e.preventDefault();
-        searchBook();
+        setTriggerSearch(true);
     }
 
     return (
